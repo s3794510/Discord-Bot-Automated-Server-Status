d029cd1c3b8e06ed8506ef5543f743d66c644375
Tung <62049425+s3794510@users.noreply.github.com>
2023-07-06 09:06:34 +0700

    add bot controling instances

--- botinstancecontrol.py
+++ botinstancecontrol.py
@@ -1,6 +1,28 @@
 import boto3
 
 
+def start_instance(instance_id):
+    ec2 = boto3.client('ec2')
+    response = ec2.start_instances(InstanceIds=[instance_id])
+    print(f"Starting instance {instance_id}")
+    return(response)
+
+def stop_instance(instance_id):
+    ec2 = boto3.client('ec2')
+    response = ec2.stop_instances(InstanceIds=[instance_id])
+    print(f"Stopping instance {instance_id}")
+    return(response)
+
+def get_instance_name(instance):
+    for tag in instance.tags:
+        if tag['Key'] == 'Name':
+            return tag['Value']
+    return ''
+
+def list_instances():
+    ec2 = boto3.resource('ec2')
+    instances = ec2.instances.all()
+    
     instance_list = []
     for instance in instances:
         instance_id = instance.id
